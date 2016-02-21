80bdef3fd2735dba7b4916e776f0acb8b2ed9e68
Karrlem <karrlemlewis@yahoo.com>
2016-02-21 16:03:01 -0500

    Added to Git

---
+++ ProgrammingChallenge1/src/challenge.java
@@ -0,0 +1,29 @@
+
+public class challenge {
+
+	public static void main(String[] args){
+		 String name;
+		 int age;
+		 double annualPay;
+		 
+		 name = "Karrlem Lewis";
+		 age = 23;
+		 annualPay = 1300000;
+				 
+		System.out.println("My name is " + name + ", my age is " + age + ", and I hope to earn $" + annualPay + " per year.");
+	}
+}
+
+/*
+ 1. Name, Age, and Annual Income
+Write a program that declares the following:
+• a String variable named name
+• an int variable named age
+• a double variable named annualPay
+
+Store your age, name, and desired annual income as literals in these variables. The program
+should display these values on the screen in a manner similar to the following:
+
+My name is Joe Mahoney, my age is 26 and
+I hope to earn $100000.0 per year.
+*/
