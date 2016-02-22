2f4467790c68716e915a83984d96cfa9cceab9c8
Karrlem <karrlemlewis@yahoo.com>
2016-02-21 21:15:25 -0500

    Fixed names

--- ProgrammingChallenge/src/challenge1.java
+++ ProgrammingChallenge/src/challenge1.java
@@ -1,5 +1,5 @@
 
-public class challenge {
+public class challenge1 {
 
 	public static void main(String[] args){
 		 String name;
---
+++ ProgrammingChallenge/src/challenge2.java
@@ -0,0 +1,23 @@
+
+public class challenge2 {
+
+	public static void main(String[] args){
+	
+		System.out.println("Karrlem Lewis\n");
+		System.out.print("Somewhere St Brooklyn, NY, 11233\n");
+		System.out.print("347-123-1234\n");
+		System.out.print("Multimedia Programming\n");
+		
+		
+	}
+}
+
+
+/*Write a program that displays the following information, each on a separate line:
+• Your name
+• Your address, with city, state, and ZIP
+• Your telephone number
+• Your college major
+Although these items should be displayed on separate output lines, use only a single println
+statement in your program.
+ */
